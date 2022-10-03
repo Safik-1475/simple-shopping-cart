81633fffc7e4b3e0641a39fd9e5cfe0dc3354ed5
Safi <safikurrahman7514@gmail.com>
2022-10-03 20:38:53 +0600

    complete case plus minus button

--- js/vanilla.js
+++ js/vanilla.js
@@ -6,7 +6,7 @@
    ================================
 */
 
-// Get HTML Plus and Minus Button With getElementById
+// Get Phone Plus and Minus Button With getElementById
 const phonePlusBtn = document.getElementById('phone-plus');
 const phoneMinusBtn = document.getElementById('phone-minus');
 // console.log(phonePlusBtn);
@@ -35,9 +35,44 @@ phonePlusBtn.addEventListener('click', function () {
     updateIphone11PriceIncreaseDecrease('phone', true)
 
 });
-
 // Handle Phone Minus Button Function
 phoneMinusBtn.addEventListener('click', function () {
     updateIphone11PriceIncreaseDecrease('phone', false)
 
+});
+
+
+// Handle Phone Case 
+// Get Case Plus and Minus Button With getElementById
+const casePlusBtn = document.getElementById('case-plus');
+const caseMinusBtn = document.getElementById('case-minus');
+// console.log(casePlusBtn)
+// console.log(caseMinusBtn)
+
+// Update Case Price
+function updateCasePriceIncreaseDecrease(product, isIncrease) {
+    // Get Case input field
+    const caseInput = document.getElementById(product + '-input');
+    // console.log(caseInput);
+    let caseInputValue = parseInt(caseInput.value);
+    // console.log(caseInputValue);
+    if (isIncrease) {
+        caseInputValue = caseInputValue + 1;
+    } else if (caseInputValue > 0) {
+        caseInputValue = caseInputValue - 1;
+    };
+    caseInput.value = caseInputValue;
+
+    // Get The Case Current Price
+    const currentCasePrice = document.getElementById('case-price');
+    // console.log(currentCasePrice);
+    currentCasePrice.innerText = caseInputValue * 59;
+}
+// Handle Case Plus Event Button Function
+casePlusBtn.addEventListener('click', function () {
+    updateCasePriceIncreaseDecrease('case', true)
+});
+// Handle Case Minus Event Button Function
+caseMinusBtn.addEventListener('click', function () {
+    updateCasePriceIncreaseDecrease('case', false)
 });
